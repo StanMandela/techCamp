332b34ba0ab15eb05aade8ab058215a8fdb59ff6
StanMandela <38228021+StanMandela@users.noreply.github.com>
2019-11-19 12:44:51 +0300

    running python

---
+++ myWebApp/hello.py
@@ -0,0 +1,54 @@
+
+print ("hello world")
+
+#variable
+name ="mandela"
+print(name)
+age = 30
+gender="male"
+married = True
+
+# print(married)
+# name="cecil"
+# print(type(name))
+#
+# print(type(age))
+# print(type(married))
+
+status='Monday November 18'
+#indexing
+new_var=status[3]
+#slicing - getting characters of a certain range
+
+new_var=status[0:] #from the specified index to yhe end of the string
+new_var2=status[9:13]
+new_var3=status[9:17]
+#
+# print(new_var)
+# print(new_var2)
+# print(new_var3)
+
+# length of the whole statement
+string = "Python is awesome, isn't it?"
+x = len(string)
+print("The total number of characters is:", x)
+
+#counting a Substring in a sentence
+substring = " "
+count = string.count(substring)
+print("The count is:", count)
+print(" For the exercise")
+
+py= "We are learning how to program in python. I find python programming fun"
+substring=" python"
+count2= py.count(substring)
+print("The count is:", count2)
+
+print(" Write a Python function to reverses the word below: raven")
+word="madamSOSmadam"
+y= word[-1::-1] # [-1::-1] here we have three arguments
+    # first is -1 that means start from last element
+    # second argument is empty that means move to end of list
+    # third arguments is difference of step
+print(y)
+
