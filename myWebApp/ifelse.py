e88f1af1065868cbab41b9d0ae2867e1c2dbeccd
StanMandela <38228021+StanMandela@users.noreply.github.com>
2019-11-20 20:23:36 +0300

    python day 3

---
+++ myWebApp/ifelse.py
@@ -0,0 +1,34 @@
+
+
+
+"""
+if...else statement
+
+"""
+number=100
+
+# if number>50:
+#     print("Passed")
+
+#if...else
+if number>150:
+    print("Passed")
+else:
+    print("Failed")
+
+#if...elseif(elif)..
+num = int(input("Enter a Number"))
+if num >0:
+    print("{} is > than".format(num))
+elif num ==10:
+    print("{} is > than".format(num))
+else:print("{} is your marks".format(num))
+
+
+#
+# x = int(input("Enter a Number"))
+#
+# if x >0:
+# print("Your Number is a Positive Number")
+# else:
+# print("You have entered a Negative number")
